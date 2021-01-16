796233fc8f3026d45bb564d828a1fd6f87c2c308
jameschu <jameschu@umich.edu>
2021-01-16 01:35:27 -0500

    Begun adding the pages to the app.py. Also, began with logic for sign up and log in and will update front end stuff later.

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from stellar_sdk import Server, Asset, Account, Keypair, TransactionBuilder, Net
 # -- Initialization section --
 app = Flask(__name__)
 
-app.secret_key = "ojI3jkol:#Me]_mk2mkm@mMWr3"
-user = os.environ['user']
-pw = os.environ['pw']
-
-# name of database
+app.secret_key = "ojI3jkol:#Me]_mk2mkm@mMWr3" 
+user = os.environ['user'] 
+pw = os.environ['pw'] 
+ 
+# name of database 
 app.config['MONGO_DBNAME'] = 'HTN21'
 
 # URI of database
@@ -29,46 +29,113 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html')
 
-@app.route('/signup', methods=["GET", "POST"])
-def signup(): 
-    return "To Do"
+# CONNECT TO DB, ADD DATA
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/signup', methods=["GET","POST"]) 
+def signup():
+    if session: 
+        return redirect(url_for('/explore'))
+    if request.method != "GET":
+        user_name = request.form["fullName"] 
+        user_email = request.form["email"]
+        user_password = request.form["password"]
+        user_public_key = request.form["publicKey"]
+        if model.verify_destination(user_public_key) is False: 
+            return "Your public key doesn't exist. Please try again"
+        user_private_key = request.form["privateKey"]
+        data_user_info = mongo.db.users
+        user_info = data_user_info.find({})
+        userData = []
+        for i in user_info: 
+            userData.append(i)
+        existing_user = data_user_info.find_one({'email': user_email})
+        if existing_user is None: 
+            new_id = False
+            user_id = 0
+            while new_id is False: 
+                random.seed(time.time())
+                user_id = round(random.random()*10000000000)
+                exists = data_user_info.find_one({'id':user_id})
+                if exists is None:
+                    new_id = True
+            data_user_info.insert({"name":user_name, "email":user_email, "password": str(bcrypt.hashpw(user_password.encode("utf-8"), bcrypt.gensalt()), 'utf-8'),
+            "public_key": user_public_key, "private_key": user_private_key, "id":user_id})
+            session["user"] = user_id
+            return redirect(url_for('explore'))
+        else: 
+            return 'That email already exists! Try logging in.'
+    else: 
+        return render_template('signup.html')
+
+@app.route('/login', methods = ["GET","POST"])
 def login(): 
-    return "To Do"
+    if session: 
+        return redirect(url_for('explore'))
+    if request.method != "GET": 
+        user_email = request.form["email"]
+        user_password = request.form["password"]
+        data_user_info = mongo.db.users 
+        user_info = data_user_info.find({})
+        userData = []
+        for i in user_info: 
+            userData.append(i)
+        login_user = data_user_info.find_one({'email': user_email}) 
+        if login_user is None: 
+            return ("It seems like you do not have an account. Please type your email correctly or sign up.")
+        elif(user_email == login_user["email"]) and (bcrypt.hashpw(user_password.encode('utf-8'), login_user['password'].encode('utf-8')) == login_user["password"].encode('utf-8')):
+            session["user"] = login_user['id']           
+            return redirect(url_for('explore'))          
+        return "Invalid combination! Please try again."
+    else: 
+        return render_template('login.html')
 
-@app.route('/explore')
+@app.route('/explore') 
 def explore(): 
-    return "To Do"
+    fundraisers_info = mongo.db.fundraisers 
+    fundraisers = fundraisers_info.find({})
+    return render_template('explore.html', fundraisers=fundraisers)
 
 @app.route('/CreateFundraiser', methods=["GET","POST"])
 def CreateFundraiser():
-    return "To Do"
+    return render_template('CreateFundraiser.html')
 
 @app.route('/f/<fundraiser_id>')
 def f(fundraiser_id):
-    return "To Do"
+    return render_template('f.html')
 
 @app.route('/donate/<fundraiser_id>', methods = ["GET","POST"])
 def donate(fundraiser_id):
-    return "To Do"
+    return render_template('donate.html')
 
 @app.route('/logout')
 def logout():
     session.clear()
-    return "To Do"
+    return "Back to home page <a href='/index'>here</a>." 
+
 
 @app.route('/deleteAccount', methods=["GET","POST"])
 def deleteAccount(): 
-    return "To Do"
+    if session: 
+        users = mongo.db.users  
+        users.delete_one({'id':session['id']})
+        session.clear() 
+        return "Your account has been deleted. Return to the home page <a href='/index'>here</a>"
+    else: 
+        return "You are not signed in. If you would like to delete your account please sign in."
 
 @app.route('/editProfile', methods=["GET","POST"])
 def editProfile(): 
-    return "To Do"
+    if session: 
+        users = mongo.db.users
+        user = users.find_one({"id":session["user"]})
+        if not user: 
+            return url_for('/login')
+        else: 
+            return "<h3> PAGE UNDER CONSTRUCTION, IF YOU NEED TO EDIT YOUR PROFILE PLEASE CONTACT US OR DELETE YOUR ACCOUNT <a href='/deleteAccount'>HERE</a>!!!!"
 
 @app.route('/history')
 def history(): 
-    return "To Do"
+    return render_template('history.html')
 
 
 
