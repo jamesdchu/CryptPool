b5d26ceb9ae85e995dfa335ca50a6d6224e0cca0
jameschu <jameschu@umich.edu>
2021-01-17 02:18:27 -0500

    App py almost done hopefully

--- app.py
+++ app.py
@@ -95,33 +95,99 @@ def explore():
     fundraisers = fundraisers_info.find({})
     return render_template('explore.html', fundraisers=fundraisers)
 
-@app.route('/CreateFundraiser', methods=["GET","POST"])
-def CreateFundraiser():
-    return render_template('CreateFundraiser.html')
+@app.route('/CreateFundraiser', methods = ["GET","POST"])
+def CreateFundraiser(): 
+    if session:
+        if request.method == "POST": 
+            fundraisers = mongo.db.fundraisers
+            title = request.form["title"]
+            date = request.form["date"]
+            description = request.form["description"]
+            location = request.form["location"]
+            category = request.form["category"] 
+            # ADD AN ID GENERATOR
+            new_id = False
+            while new_id is False: 
+                random.seed(time.time())
+                id_fundraiser = round(random.random()*10000000000)
+                exists = fundraisers.find_one({'id':id_fundraiser})
+                if exists is None:
+                    new_id = True
+            fundraisers.insert({"title":title, "date":date, "description":description, 
+                                "location":location, "category":category, "id":id_fundraiser,
+                                "creator":session["user"]})
+            return redirect(url_for('explore'))  
+        else: 
+            return render_template("CreateFundraiser.html")
+    else: 
+        return redirect(url_for('/login'))
 
-@app.route('/f/<fundraiser_id>')
-def f(fundraiser_id):
-    return render_template('f.html')
+
+@app.route('/f/<fundraiser_id>') 
+def f(fundraiser_id):  
+    fundraisers = mongo.db.fundraisers 
+    f_id = int(fundraiser_id)
+    fundraiser = fundraisers.find_one({'id':f_id}) 
+    
+    donations_all = mongo.db.donations
+    donations = donations_all.find({'fundraiser':fundraiser_id}) 
+
+    if not fundraiser: 
+        return "Sorry, no profile. <a href='/explore'>Return to the explore page</a>"
+    return render_template('f.html', **locals())
 
 @app.route('/donate/<fundraiser_id>', methods = ["GET","POST"])
-def donate(fundraiser_id):
-    return render_template('donate.html')
+def donate(fundraiser_id): 
+    if session: 
+        fundraisers = mongo.db.fundraisers
+        f_id = int(fundraiser_id)
+        fundraiser = fundraisers.find_one({'id':int(f_id)})
+        users = mongo.db.users 
+        user = users.find_one({'id':int(session["user"])})
+        if not user:
+                return redirect(url_for('/login'))
+        # return str(session['user'])
+        if not fundraiser: 
+            return "Sorry, no profile. <a href='/explore'>Return to the explore page</a>"
+        
+        if request.method == "POST": 
+        
+            donations = mongo.db.donations
+            display_name = request.form["name"] 
+            asset = request.form["asset"]  
+            quantity = request.form["quantity"] 
+            message = request.form["message"]  
+            donator_id = session["user"] 
+
+            donator_sKey = user['private_key']
+            creator_fundraiser = users.find_one({'id':fundraiser['creator']})
+
+            fundraiser_pKey = creator_fundraiser['public_key']
+
+            if (model.sendPayment(donator_sKey, fundraiser_pKey, quantity, asset, message, str(donator_id), str(fundraiser['id'])) == False): 
+                return "Sorry, the transaction failed. Please try again or check your assets."
+            donations.insert({"display":display_name,"fundraiser":fundraiser_id,"donator":donator_id,
+                                "asset":asset,"quantity":int(quantity),"message":message})
+            return redirect(url_for('history'))
+        else: 
+            return render_template('donate.html', **locals()) 
+    else: 
+        return redirect(url_for('/login'))
 
 @app.route('/logout')
-def logout():
-    session.clear()
+def logout(): 
+    session.clear() 
     return "Back to home page <a href='/index'>here</a>." 
-
 
 @app.route('/deleteAccount', methods=["GET","POST"])
 def deleteAccount(): 
     if session: 
-        users = mongo.db.users  
-        users.delete_one({'id':session['id']})
+        users = mongo.db.users 
+        users.delete_one({"id":session["id"]})
         session.clear() 
         return "Your account has been deleted. Return to the home page <a href='/index'>here</a>"
     else: 
-        return "You are not signed in. If you would like to delete your account please sign in."
+        return "You aren't signed in! If you would like to delete your account, please log in <a href='/login'>here!</a>"
 
 @app.route('/editProfile', methods=["GET","POST"])
 def editProfile(): 
@@ -135,16 +201,39 @@ def editProfile():
 
 @app.route('/history')
 def history(): 
-    return render_template('history.html')
+    if session: 
+        users = mongo.db.users
+        user = users.find_one({"id":session["user"]})  
+        donations_all = mongo.db.donations  
+        donations = donations_all.find({})  
+        fundraisers_all = mongo.db.fundraisers
+        fundraisers = fundraisers_all.find({})
+        my_fundraisers = []
+        my_balances = []
+        donated = []
 
+        for fundraiser in fundraisers: 
+            if(fundraiser['creator'] == user['id']):
+                my_fundraisers.append([fundraiser['title'], fundraiser['id']]) # TAKES THE FUNDRAISERS THATY YOU CREATED
 
+        for donation in donations:  
+            for fundraiser in my_fundraisers: 
+                if(int(donation['fundraiser']) == fundraiser[1]):  
+                    for asset_type in my_balances:   
+                        if(asset_type[0] == donation['asset']):   
+                            asset_type[1]=str(int(donation['quantity'])+int(asset_type[1]))   
+                        else: 
+                            my_balances.append([donation['asset'],donation['quantity']])  
+    
+            if(donation['donator']==session['user']): 
+                donated_fundraiser = fundraisers_all.find_one({'id':int(donation['fundraiser'])})  
+                donated.append([donated_fundraiser['title'], donation['asset'], donation['quantity']]) #TAKES THE TITLE, ASSET TYPE, and QUANTITY 
 
-
-
-
+        return render_template('history.html', **locals()) 
+    else:  
+        return render_template('index.html')         
 
 # @app.route('/add')
-
 # def add():
 #     # connect to the database
 
